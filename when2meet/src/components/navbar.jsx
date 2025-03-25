1a1412eaaa20c1010581057724d4323ab07bb3f4
RyanXiaoo <118212529+RyanXiaoo@users.noreply.github.com>
2025-03-24 21:30:53 -0400

    homepages updated

--- when2meet/src/components/navbar.jsx
+++ when2meet/src/components/navbar.jsx
@@ -2,27 +2,29 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div className="flex justify-center items-center">
-            <nav className="bg-gray-800 flex justify-around items-center p-4 w-1/4 rounded-full">
-                <Link
-                    to="/"
-                    className="text-white hover:text-gray-300 transition-colors p-1 rounded-full"
-                >
-                    Home
-                </Link>
-                <Link
-                    to="/login"
-                    className="text-white hover:text-gray-300 transition-colors p-1 rounded-full"
-                >
-                    Login
-                </Link>
-                <Link
-                    to="/sign-up"
-                    className="text-white hover:text-gray-300 transition-colors p-1 rounded-full"
-                >
-                    Sign Up
-                </Link>
-            </nav>
+        <div className="fixed top-0 left-0 right-0 z-50 py-4">
+            <div className="flex justify-center items-center">
+                <nav className="bg-gray-800 flex justify-around items-center p-4 w-1/4 rounded-full">
+                    <Link
+                        to="/"
+                        className="text-white hover:text-gray-300 transition-colors p-1 rounded-full"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/login"
+                        className="text-white hover:text-gray-300 transition-colors p-1 rounded-full"
+                    >
+                        Login
+                    </Link>
+                    <Link
+                        to="/sign-up"
+                        className="text-white hover:text-gray-300 transition-colors p-1 rounded-full"
+                    >
+                        Sign Up
+                    </Link>
+                </nav>
+            </div>
         </div>
     );
 }
--- when2meet/src/pages/loggedout/home.jsx
+++ when2meet/src/pages/loggedout/home.jsx
@@ -1,7 +1,11 @@
+import calendar from "../../images/calendar.svg";
+import friends from "../../images/friends.svg";
+import perfecttime from "../../images/perfecttime.svg";
+
 export default function Home() {
     return (
-        <div className="flex flex-col items-center h-[calc(100vh-4rem)] w-screen">
-            <div className="flex flex-col items-center bg-gray-800 p-8 rounded-lg shadow-lg w-2/3 m-8">
+        <div className="flex flex-col items-center w-screen pb-8">
+            <div className="flex justify-center flex-col items-center rounded-lg w-2/3 min-h-screen">
                 <h1 className="text-white text-6xl font-bold">
                     Welcome to When2Meet
                 </h1>
@@ -10,16 +14,16 @@ export default function Home() {
                 </p>
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6  px-6 text-white">
-                    <div className="bg-gray-700 p-6 rounded-lg shadow-md">
+                    <div className="bg-gray-700/60 p-6 rounded-lg shadow-md">
                         <h2 className="text-xl font-bold mb-2">
-                            See everyone’s availability, all in one place
+                            See everyone's availability, all in one place
                         </h2>
                         <p>
                             Link your calendar and instantly check when friends
                             are free — no guessing, no group texts.
                         </p>
                     </div>
-                    <div className="bg-gray-700 p-6 rounded-lg shadow-md">
+                    <div className="bg-gray-700/60 p-6 rounded-lg shadow-md">
                         <h2 className="text-xl font-bold mb-2">
                             Connect your Google Calendar or Notion
                         </h2>
@@ -29,7 +33,7 @@ export default function Home() {
                             times.
                         </p>
                     </div>
-                    <div className="bg-gray-700 p-6 rounded-lg shadow-md">
+                    <div className="bg-gray-700/60 p-6 rounded-lg shadow-md">
                         <h2 className="text-xl font-bold mb-2">
                             We find the best time for you and your friends
                         </h2>
@@ -38,6 +42,81 @@ export default function Home() {
                             select a few friends and you won't have to figure
                             this out
                         </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center p-2 rounded-lg shadow-lg w-2/3 m-8 min-h-screen">
+                <div className="mt-48 p-8 flex flex-col w-full">
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-white text-4xl p-4">
+                            How it works
+                        </h1>
+                    </div>
+
+                    <div className="flex flex-col items-center bg-gray-700/60 w-2/3 rounded-full m-4 h-1/5">
+                        <div className="flex flex-row items-center justify-evenly h-full">
+                            <div className="flex flex-col items-center w-1/3">
+                                <img
+                                    src={calendar}
+                                    alt="calendar"
+                                    className="w-full p-8"
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <h2 className="text-white text-2xl pl-4">
+                                    1. Link your calendar
+                                </h2>
+                                <p className="text-white text-lg p-4">
+                                    Import your schedule once and stay synced
+                                    automatically. Everyone sees the most
+                                    up-to-date times.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex self-end flex-col items-center bg-gray-700/60 w-2/3 rounded-full m-4 h-1/5">
+                        <div className="flex flex-row items-center justify-evenly h-full p-4">
+                            <div className="flex flex-col items-center w-1/2">
+                                <img
+                                    src={friends}
+                                    alt="friends"
+                                    className="w-full p-8"
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <h2 className="text-white text-2xl pl-4">
+                                    2. Select your friends
+                                </h2>
+                                <p className="text-white text-lg p-4">
+                                    Invite your friends to connect. Their
+                                    schedules sync instantly so you can all see
+                                    when everyone's available
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex  flex-col items-center bg-gray-700/60 w-2/3 rounded-full m-4 h-1/5">
+                        <div className="flex flex-row items-center justify-evenly h-full p-4">
+                            <div className="flex flex-col items-center w-1/2">
+                                <img
+                                    src={perfecttime}
+                                    alt="perfecttime"
+                                    className="w-full p-8"
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <h2 className="text-white text-2xl pl-4">
+                                    3. Find the perfect time
+                                </h2>
+                                <p className="text-white text-lg p-4">
+                                    We’ll highlight the best overlapping times
+                                    for your group. Just pick what works and
+                                    you're good to go.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
